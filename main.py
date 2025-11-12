b91db8a8329959cc4e77537c6434ee9ba4c38bb1
BeyzaGonculer <beyza.gonculer1@gmail.com>
2025-11-12 08:55:05 +0300

    load and print simple image updated

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ def load_sample_rgb():
     # skimage.data.astronaut() RGB döndürür (uint8)
     rgb = data.astronaut()
     return rgb
+
 def imshow_rgb(img, title=None):
     plt.imshow(img)
-    if title: plt.title(title)
+    if title: plt.title(title) #
     plt.axis("off")
     plt.show()
 
@@ -26,19 +27,12 @@ def main():
 if __name__ == "__main__":
     main()
 
-
-
-
-
-plt.rcParams["figure.figsize"] = (6, 4)
-
+plt.rcParams["figure.figsize"] = (6, 4) # açılan pencerenin boyutu
 
 print("Part 1.1")
+print("Step 1")
 rgb = load_sample_rgb()
 print("Image shape:", rgb.shape, "dtype:", rgb.dtype)
-
-print("ECE530 A1 - Part 1.1")
-rgb = load_sample_rgb()
 imshow_rgb(rgb, "Input RGB (astronaut)")
 
 
